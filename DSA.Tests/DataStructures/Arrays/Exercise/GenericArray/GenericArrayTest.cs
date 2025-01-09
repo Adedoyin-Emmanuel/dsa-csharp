9107750c4ab9352f9fd0aac993df5a6263c32d98
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-01-09 11:08:35 +0100

    feat: CountShouldReturnCorrectValueAfterInsertingElements test passed

--- DSA.Tests/DataStructures/Arrays/Exercise/GenericArray/GenericArrayTest.cs
+++ DSA.Tests/DataStructures/Arrays/Exercise/GenericArray/GenericArrayTest.cs
@@ -1,6 +1,26 @@
+using DSA.DSA.DataStructures.Arrays.Exercise.GenericArray;
+
 namespace DSA.Tests.DataStructures.Arrays.Exercise.GenericArray;
 
 public class GenericArrayTest
 {
+    private GenericArray<int> _numbers;
     
+    [SetUp] 
+    public void SetUp()
+    {
+        _numbers = new GenericArray<int>();
+    }
+
+    [Test]
+    public void CountShouldReturnCorrectValueAfterInsertingElements()
+    {
+        _numbers.Insert(1);
+        _numbers.Insert(2);
+        _numbers.Insert(3);
+        _numbers.Insert(4);
+        
+        
+        Assert.That(_numbers.Count(), Is.EqualTo(4));
+    }
 }
